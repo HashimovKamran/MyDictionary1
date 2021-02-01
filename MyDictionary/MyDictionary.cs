2163886acee3830f9ff4ccbd1b781c77df1baf02
Hp <kamranhesimov9@gmail.com>
2021-02-02 03:26:57 +0400

    Добавьте файлы проекта.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TK, TV>
+    {
+        TK[] _keyArray;
+        TV[] _valueArray;
+
+        TK[] _tempKeyArray;
+        TV[] _tempValueArray;
+
+        public MyDictionary()
+        {
+            _keyArray = new TK[0];
+            _valueArray = new TV[0];
+        }
+
+        public void Add(TK key, TV value)
+        {
+            _tempKeyArray = _keyArray;
+            _tempValueArray = _valueArray;
+
+            _keyArray = new TK[_keyArray.Length + 1];
+            _valueArray = new TV[_valueArray.Length + 1];
+
+            for (int i = 0; i < _tempKeyArray.Length; i++)
+            {
+                _keyArray[i] = _tempKeyArray[i];
+            }
+            for (int i = 0; i < _tempValueArray.Length; i++)
+            {
+                _valueArray[i] = _tempValueArray[i];
+            }
+
+            _keyArray[_keyArray.Length - 1] = key;
+            _valueArray[_valueArray.Length - 1] = value;
+        }
+        public int Count
+        {
+            get { return _keyArray.Length; }
+        }
+
+        public TK[] Keys
+        {
+            get { return _keyArray; }
+        }
+        public TV[] Values
+        {
+            get { return _valueArray; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> kullanici = new MyDictionary<int, string>();
+            kullanici.Add(0, "Kerem");
+            kullanici.Add(1, "Ali");
+            kullanici.Add(2, "Aykut");
+            kullanici.Add(3, "Birkan");
+            kullanici.Add(4, "Kaan");
+
+            for (int i = 0; i < kullanici.Count; i++)
+            {
+                Console.WriteLine(kullanici.Keys[i] + " " + kullanici.Values[i]);
+            }
+        }
+    }
+}
